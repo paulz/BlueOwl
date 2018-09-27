d2bbd7fa2f37e8894b8d5696ff959d7e35ac7fba
Paul Zabelin <pzabelin+git@gmail.com>
2018-09-27 16:13:03 -0700

    add dynamic swift framework target to project
    in order to modularize app

---
+++ SwiftOwl/SwiftOwl.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOwl.h
+//  SwiftOwl
+//
+//  Created by Paul Zabelin on 9/27/18.
+//  Copyright © 2018 Paul Zabelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOwl.
+FOUNDATION_EXPORT double SwiftOwlVersionNumber;
+
+//! Project version string for SwiftOwl.
+FOUNDATION_EXPORT const unsigned char SwiftOwlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOwl/PublicHeader.h>
+
+
